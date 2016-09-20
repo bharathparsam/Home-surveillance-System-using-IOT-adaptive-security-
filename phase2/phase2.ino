49fc528304c49e0666b7f8ab2ffed54998adc568
Bharath Parsam <bharathparsam@live.com>
2016-09-20 16:27:35 +0530

    Add files via upload
    Phase-1:
    Automatic Lighting of lights based on the object detection
    Phase-2:
    Led is lightened when motion is detected

---
+++ phase2/phase2.ino
@@ -0,0 +1,32 @@
+int led = 13;                // Pin for LED attachment
+int sensor = 2;              // Sensor pin attachment 
+int state = LOW;             // Defaultly no motion is detected
+int val = 0;                 // Store the Sensor Values
+
+void setup() {
+  pinMode(led, OUTPUT);      // Intilization of LED Output
+  pinMode(sensor, INPUT);    // Intilization of Sensor Value INPUT
+  Serial.begin(9600);        // Serail pin intilazion
+}
+
+void loop(){
+  val = digitalRead(sensor);   // reading the sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    delay(10);                // delay 100 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update the variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      delay(200);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+}
